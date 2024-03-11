ad438d7b0b647ffa298e7d3b26fc62177f513b18
gupta <sairohitgupta1999@gmail.com>
2024-03-11 19:42:00 +0530

    Was blackmailed by other neighbour kids to add their voice.

--- script.js
+++ script.js
@@ -10,22 +10,23 @@ function playRandomAudio() {
         2: "Sholder.mp3",
         3: "knee.mp3",
         4: "toe.mp3",
-        5: "Bottle.mp3"
+        5: "Hips.mp3",
+        6: "Bottle.mp3"
     };
 
     // Generate a random option
-    const choice = Math.floor(Math.random() * 5) + 1;
+    const choice = Math.floor(Math.random() * 6) + 1;
     console.log("Selected option:", choice);
 
     // Play the audio for the selected option
     const audio = new Audio(audioFiles[choice]);
     audio.onended = function() {
         // After the audio finishes playing, check if option 5 was selected
-        if (choice !== 5) {
+        if (choice !== 6) {
             // If not, play the next random audio
             playRandomAudio();
         } else {
-            // If option 5 was selected, re-enable the button
+            // If option 6 was selected, re-enable the button
             document.getElementById("startButton").disabled = false;
         }
     };
